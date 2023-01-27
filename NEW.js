7f22a9be5d3de1bf0cf4df62bdd5a2345823d595
dule88 <d.dimitrijevic88@gmail.com>
2023-01-28 00:47:27 +0100

    Updating Movie-App to be more user friendly

--- NEW.js
+++ NEW.js
@@ -1,16 +1,16 @@
-const btnSearch = document.querySelector('.btnSearch');
-const searchInput = document.querySelector('.searchInput');
-const resultOfSearch = document.querySelector('.resultOfSearch');
-const cartElement = document.querySelector('.cart');
-const leftElement = document.querySelector('.left');
-const rightElement = document.querySelector('.right');
-const currentPageElement = document.querySelector('.currentPage');
+const btnSearch = document.querySelector('#searchBtn');
+const searchInput = document.querySelector('#searchInput');
+const resultOfSearch = document.querySelector('#resaultContainer');
+// const cartElement = document.querySelector('#resaultContainer');
+// const leftElement = document.querySelector('.left');
+// const rightElement = document.querySelector('.right');
+// const currentPageElement = document.querySelector('.searchPagination');
 
 
 
 let movieState = [];
 let cartMovies = [];
-let currentPage = 1;
+// let currentPage = 1;
 
 const setMovieState = (list) => {
   movieState = [...list];
@@ -20,9 +20,9 @@ const setCartMovies = (list) => {
   cartMovies = [...list];
 }
 
-const setCurrentPage = (num)  => {
-  currentPage = num;
-}
+// const setCurrentPage = (num)  => {
+//   currentPage = num;
+// }
 
 
 // Event listener for searching the movies
@@ -35,8 +35,8 @@ const setCurrentPage = (num)  => {
 
       // function for fetching the movies from API
       const fetchMovie = () => {
-        currentPageElement.value = currentPage;
-        fetch( `https://www.omdbapi.com/?apikey=56e00a84&s=${searchInput.value}&page=${currentPage}` )
+        // currentPageElement.value = currentPage;
+        fetch( `https://www.omdbapi.com/?apikey=56e00a84&s=${searchInput.value}` )
         .then(res => res.json())
         .then(movies => {
           setMovieState(movies.Search);
@@ -68,50 +68,50 @@ const setCurrentPage = (num)  => {
       }
 
 // Function for adding movies into the cart
-      const add = (idx) => {
-        setCartMovies([...cartMovies, movieState[idx]]);
-        cartRender();
-      }
+      // const add = (idx) => {
+      //   setCartMovies([...cartMovies, movieState[idx]]);
+      //   cartRender();
+      // }
 
-// Function for rendering Movies into the cart
-      const cartRender = () => {
+// // Function for rendering Movies into the cart
+//       const cartRender = () => {
 
-        cartElement.innerHTML = '';
+//         cartElement.innerHTML = '';
 
-        cartMovies.forEach((movie, idx) => {
-          cartElement.innerHTML += 
-          `<div class="col text-dark m-2 pb-4">
-            <div class="card" style="width: 18rem; height:100%;">
-              <img src=${movie.Poster} class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${movie.Title}</h5>
-                <p class="card-text">Year: ${movie.Year} Type: ${movie.Type}</p>
-                <a class="btn btn-danger" onClick="deletefunction(${idx})">Delete</a>
-              </div>
-            </div>
-          </div>`;
-        });
-      }
+//         cartMovies.forEach((movie, idx) => {
+//           cartElement.innerHTML += 
+//           `<div class="col text-dark m-2 pb-4">
+//             <div class="card" style="width: 18rem; height:100%;">
+//               <img src=${movie.Poster} class="card-img-top" alt="...">
+//               <div class="card-body">
+//                 <h5 class="card-title">${movie.Title}</h5>
+//                 <p class="card-text">Year: ${movie.Year} Type: ${movie.Type}</p>
+//                 <a class="btn btn-danger" onClick="deletefunction(${idx})">Delete</a>
+//               </div>
+//             </div>
+//           </div>`;
+//         });
+//       }
 
-      // Function for deleting movies from the cart
-      const deletefunction = (idx) => {
-        let temp = [...cartMovies];
-        temp.splice(idx, 1);
-        setCartMovies(temp);
-        cartRender();
-      }
+      // // Function for deleting movies from the cart
+      // const deletefunction = (idx) => {
+      //   let temp = [...cartMovies];
+      //   temp.splice(idx, 1);
+      //   setCartMovies(temp);
+      //   cartRender();
+      // }
 
-      //left Event listener for pagination
-      leftElement.addEventListener('click', () => {
-        currentPage <=1 ? setCurrentPage(1) : setCurrentPage(currentPage - 1);
-        setMovieState([]);
-        fetchMovie();
-        render();
-      });
-      //right Event listener for pagination
-      rightElement.addEventListener('click', () => {
-        setCurrentPage(currentPage + 1);
-        setMovieState([]);
-        fetchMovie();
-        render();
-      });
+      // //left Event listener for pagination
+      // leftElement.addEventListener('click', () => {
+      //   currentPage <=1 ? setCurrentPage(1) : setCurrentPage(currentPage - 1);
+      //   setMovieState([]);
+      //   fetchMovie();
+      //   render();
+      // });
+      // //right Event listener for pagination
+      // rightElement.addEventListener('click', () => {
+      //   setCurrentPage(currentPage + 1);
+      //   setMovieState([]);
+      //   fetchMovie();
+      //   render();
+      // });
