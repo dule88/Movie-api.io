0f20cd1ff506d2922eee84d28fe9255741b82ea9
dule88 <d.dimitrijevic88@gmail.com>
2023-02-05 02:42:28 +0100

    Movie App almost finished, need to fix only cart, to provide info about products qty.

--- NEW.js
+++ NEW.js
@@ -2,13 +2,15 @@ const btnSearch = document.querySelector('#searchBtn');
 const searchInput = document.querySelector('#searchInput');
 const resultOfSearch = document.querySelector('#resaultContainer');
 const cartElement = document.querySelector('#modalListContent');
-// const leftElement = document.querySelector('.left');
-// const rightElement = document.querySelector('.right');
-const currentPageElement = document.querySelector('.searchPagination');
+const searchPagination = document.querySelector('#searchPagination');
+const alertMessage = document.querySelector('#alertMessage');
+// const counter = document.querySelector('#counter');
 
 
+let currentCart = {};
 
-let currentPage = 1;
+
+let page = 1;
 let prevPage = 0;
 let nextPage = 2;
 
@@ -28,9 +30,6 @@ const setCartMovies = (list) => {
   cartMovies = [...list];
 }
 
-// const setCurrentPage = (num)  => {
-//   currentPage = num;
-// }
 
 
 // Event listener for searching the movies
@@ -43,12 +42,15 @@ const setCartMovies = (list) => {
 
       // function for fetching the movies from API
       const fetchMovie = () => {
-        // currentPageElement.value = currentPage;
-        fetch( `https://www.omdbapi.com/?apikey=56e00a84&s=${searchInput.value}` )
+  
+        fetch( `https://www.omdbapi.com/?apikey=56e00a84&s=${searchInput.value}&page=${page}` )
         .then(res => res.json())
         .then(movies => {
           setMovieState(movies.Search);
           render();
+          pagination();
+          
+
         	
         })
         .catch(err => console.log(err));
@@ -73,14 +75,18 @@ const setCartMovies = (list) => {
             </div>
           </div>`;
         });
+
+        
       }
 
 // Function for adding movies into the cart
       const add = (idx) => {
         setCartMovies([...cartMovies, movieState[idx]]);
+        
         render();
         cartRender();
-        
+
+     
       }
 
 // Function for rendering Movies into the cart
@@ -97,7 +103,7 @@ const setCartMovies = (list) => {
 
                         <div>
                             <ul class="list-group list-group-flush border-dark" >
-                                <li class="list-group-item text-bg-dark fs-6 border-dark text-white-50 py-1 pe-0 pt-0">Title: <span class="text-white">${movie.Title}</span> </li>
+                                <li class="list-group-item text-bg-dark fs-4 border-dark text-white-50 py-1 pe-0 pt-0">Title: <span class="text-white">${movie.Title}</span> </li>
                                 </li>
                                 <li class="list-group-item text-bg-dark fs-6 border-dark text-white-50 py-1 pe-0">Year: <span class="text-white">${movie.Year}</span></li>
                               
@@ -110,28 +116,123 @@ const setCartMovies = (list) => {
                 </div>
                 <hr class="text-bg-dark mb-4">
             `
+            
         });
+
+        // moviesCounter();
       }
+
+
 
       // // Function for deleting movies from the cart
       const deletefunction = (idx) => {
+
         let temp = [...cartMovies];
         temp.splice(idx, 1);
         setCartMovies(temp);
         cartRender();
+        // moviesCounter();
       }
 
-      // //left Event listener for pagination
-      // leftElement.addEventListener('click', () => {
-      //   currentPage <=1 ? setCurrentPage(1) : setCurrentPage(currentPage - 1);
-      //   setMovieState([]);
-      //   fetchMovie();
-      //   render();
-      // });
-      // //right Event listener for pagination
-      // rightElement.addEventListener('click', () => {
-      //   setCurrentPage(currentPage + 1);
-      //   setMovieState([]);
-      //   fetchMovie();
-      //   render();
-      // });
+ 
+
+      // CART COUNTER
+
+    
+    //   window.addEventListener('load', () => {
+
+    //     let store = localStorage.getItem('movieList');
+    
+    //     if (store == null) {
+    //         store = localStorage.setItem('movieList', JSON.stringify(''));
+    //     }
+    
+        
+    //   moviesCounter();
+      
+        
+    // })
+
+
+
+  //   const moviesCounter = () => {
+  //     let readStorage = JSON.parse(localStorage.getItem('movieList'));
+  //     localStorage.setItem('movieList', JSON.stringify(readStorage));
+  //     counter.innerHTML = `<h3 class="text-white m-0">${readStorage.length}</h3>`
+      
+  // }
+
+
+
+
+      // PAGINATION
+
+      const pagination = () => {
+        searchPagination.innerHTML = `
+
+        <ul class="pagination m-0 border-success">
+
+            <li class="page-item ">
+                <a class="page-link bg-dark text-white-50 border-secondary" href="#" aria-label="Previous"  onclick=goToPreviousPage()>
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+
+            <li class="page-item " id='pp'><span class="page-link bg-dark border-secondary text-white-50 ">${prevPage}</span></li>
+            <li class="page-item active" ><span class="page-link bg-success border-secondary">${page}</span></li>
+            <li class="page-item " ><span class="page-link bg-dark border-secondary text-white-50">${nextPage}</span></li>
+            <li class="page-item " ><span class="page-link bg-dark border-secondary text-white-50">...</span></li>
+            <li class="page-item " ><span class="page-link bg-dark border-secondary text-white-50">${lastPage + 1}</span></li>
+
+            <li class="page-item">
+                <a class="page-link bg-dark text-white-50 border-secondary" href="#" aria-label="Next" onclick=goToNextPage()>
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+            
+        </ul>
+    `
+        
+      }
+
+
+      // Pagination - click on Next Page button
+      const goToNextPage = async () => {
+        resultOfSearch.innerHTML = '';
+    
+            page = page + 1;
+        
+            prevPage = page - 1;
+            nextPage = page + 1;
+            
+            fetchMovie();
+    
+    }
+    
+    
+    // Pagination - click on Previous Page button
+    const goToPreviousPage = async () => {
+      resultOfSearch.innerHTML = '';
+    
+        if(prevPage != 0){
+    
+            page = page - 1;
+        
+            prevPage = page - 1;
+            nextPage = page + 1;
+        
+            fetchMovie();
+        }else{
+          fetchMovie();
+            alertMessage.innerHTML = `
+                <div class="alert alert-danger alert-dismissible fade show mx-2 float my-6 z-3 text-center" role="alert">
+                    These is first page!!!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `
+    
+            setTimeout(() => {
+                alertMessage.innerHTML = '';
+            }, "1500")
+        }
+    }
